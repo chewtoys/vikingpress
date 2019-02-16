fa378ccf8a8881db9a3298c72432cf9bf2c16617
The Viking Voice <nsmsvikingvoice@northshoreschools.org>
2019-02-16 14:34:03 +0000

    Add account verification endpoint to auth router

--- server/routers/auth-router.js
+++ server/routers/auth-router.js
@@ -11,11 +11,26 @@ authRouter.post('/local', authActions.local.authenticate)
 authRouter.get('/google/start', authActions.google.start)
 authRouter.get('/google/callback', authActions.google.callback)
 
+/** Verify if an account exists */
+authRouter.post('/verify', (req, res) => {
+    /** Example account here for debugging on frontend.
+     * @todo Replace this with an actual database call.
+     */
+    let account = {
+        exists: true,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        username: 'doej',
+        provider: 'Local'
+    }
+    res.send(account)
+})
+
 module.exports = {
     /** Initialize auth router
      * @param {function} app - Express app function
      */
     fn: async function initializeAuthRouter(app) {
-        app.use('/api/auth', authRouter)
+        app.use('/api/accounts', authRouter)
     }
 }
