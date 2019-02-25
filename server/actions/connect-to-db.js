d345577df2455f8675cf6d570ad5fab76c12ce0e
The Viking Voice <nsmsvikingvoice@northshoreschools.org>
2019-02-25 04:40:41 +0000

    Simplify DB connect function

--- server/actions/connect-to-db.js
+++ server/actions/connect-to-db.js
@@ -2,11 +2,7 @@ const db = require('../services/db')
 
 module.exports = {
   /** Connect to database */
-  fn: async function connectToDB () {
-    try {
-      await db.sequelize.sync()
-    } catch (error) {
-      throw error
-    }
+  fn: async function connectToDB() {
+    return db.sequelize.sync()
   }
 }
