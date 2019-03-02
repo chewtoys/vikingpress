14d185b1ec93890fe3389cd49ba43dd0f50e2623
The Viking Voice <nsmsvikingvoice@northshoreschools.org>
2019-03-02 23:07:11 +0000

    Run Nuxt builder in both development and production environments

--- server/actions/nuxt/init.js
+++ server/actions/nuxt/init.js
@@ -1,7 +1,7 @@
 const { Nuxt, Builder } = require('nuxt')
 
 let nuxtConfig = require('../../../config/nuxt')
-nuxtConfig.dev = maroon.config.env === 'DEVELOPMENT'
+nuxtConfig.dev = maroon.config.env === 'development'
 
 module.exports = {
   /** Initialize Nuxt. */
@@ -10,13 +10,10 @@ module.exports = {
     const nuxt = new Nuxt(nuxtConfig)
 
     /**
-         * Build the Nuxt app if we're in a dev environment.
-         * If we're in production, build Nuxt before running the app.
-         */
-    if (nuxtConfig.dev) {
-      const builder = new Builder(nuxt)
-      await builder.build()
-    }
+     * Build the Nuxt app.
+     */
+    const builder = new Builder(nuxt)
+    await builder.build()
 
     /** Make Nuxt available globally. */
     app.use((req, res, next) => {
